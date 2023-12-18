c172797aaf1d024014af4f42bdc74d6811e0b4b3
Hotlolka <vsevolod.mishchenko1@mail.ru>
2023-12-18 21:12:38 +0300

    Finally!

--- Discord-Telegram_Bot/main.py
+++ Discord-Telegram_Bot/main.py
@@ -1,20 +1,49 @@
 import discord
 from datetime import datetime
 import json
+
+import asyncio
+import redis
 from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+bot = discord.Client(intents=intents)
+#@bot.command()
+#async def ping(ctx):
+    #await ctx.send('pong')
+
 
 @bot.event
-async def on_message( message):
+async def on_ready():
+    channel = bot.get_channel(1161679620597371012)
+    while True:
+        message = p.get_message()
+        if message and message["type"] not in ("subscribe", "unsubscribe"):
+            # bot.send_message(user_id, f'{message.from_user.first_name} ({datetime.fromtimestamp(message.date)}) : {message.text}')
+            await channel.send(message["data"].decode('utf-8'))
+        await asyncio.sleep(0.01)
+
+
+r= redis.Redis(host='redis', port=6379, db=0)
+r.ping()
+p=r.pubsub()
+p.subscribe ("telegram-channel")
+
+#@bot.event
+#async def on_message( message):
+ #   if message.author.bot:
+  #      return
+  #  else:
+  #      await message.channel.send('{0.author}({0.created_at}): {0.content}'.format(message))
+@bot.event
+async def on_message(message):
     if message.author.bot:
         return
     else:
-        await message.channel.send('{0.author}({0.created_at}): {0.content}'.format(message))
+        r.publish( 'discord-channel',  '{0.author}({0.created_at}): {0.content}'.format(message))
+    # r.publish( 'discord-channel',  f('{0.author}({0.created_at}): {0.content}'.format(message))
+
+
 
 with open("config.txt") as f:
     TOKEN = f.read().strip()
--- Telegram-Discord_Bot/Chatterbox.py
+++ Telegram-Discord_Bot/Chatterbox.py
@@ -1,43 +1,49 @@
 from typing import Final
 import telebot
 from datetime import datetime
+import redis
+import time
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
 
 TOKEN = None
 with open("token.txt") as f:
     TOKEN = f.read().strip()
 
-bot = telebot.TeleBot(TOKEN)
+bot = AsyncTeleBot(TOKEN)
 BOT_USERNAME: Final = '@Teldispp'
+ch_id = -1001522399768
 
+r = redis.Redis(host='redis', port=6379, db=0)
+r.ping()
+p = r.pubsub()
+p.subscribe("discord-channel")
 
 # responses
 @bot.message_handler(commands=["start"])
-def start(message):
-    bot.send_message(message.chat.id, 'Доброе утро! Cпасибо, что разбудили.')
+async def start(message):
+    ch_id = message.chat.id
+    await bot.send_message(message.chat.id, 'Доброе утро! Cпасибо, что разбудили.')
 
 @bot.message_handler(commands=["help"])
-def help(message):
-    bot.send_message(message.chat.id, 'Я здесь, чтобы помочь синхронизировать сообщения с двух чатов.(пока только повторяю)')
+async def help(message):
+    await bot.send_message(message.chat.id, 'Я здесь, чтобы помочь синхронизировать сообщения с двух чатов.(пока только повторяю)')
 
 @bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.send_message(message.chat.id, f'{message.from_user.first_name} ({datetime.fromtimestamp(message.date)}) : {message.text}')
-
-@bot.message_handler(content_types=['text', 'video'])
-def echo_video(message):
-    if message.caption == None:
-        bot.send_video(message.chat.id, message.video.file_id, caption = f'{message.from_user.first_name} ({datetime.fromtimestamp(message.date)}) : No text was in message')
-    else:
-        bot.send_video(message.chat.id, message.video.file_id, caption = f'{message.from_user.first_name} ({datetime.fromtimestamp(message.date)}) : {message.caption}')
-
-@bot.message_handler(content_types=['text', 'photo'])
-def echo_photo(message):
-    if message.caption == None:
-        bot.send_photo(message.chat.id, message.photo[0].file_id, caption = f'{message.from_user.first_name} ({datetime.fromtimestamp(message.date)}) : No text was in message')
-    else:
-        bot.send_photo(message.chat.id, message.photo[0].file_id, f'{message.from_user.first_name} ({datetime.fromtimestamp(message.date)}) : {message.caption}')
+async def receive_message(message):
+    print("we got here")
+    await r.publish('telegram-channel', f'{message.from_user.first_name} ({datetime.fromtimestamp(message.date)}) : {message.text}')
 
 
-bot.polling()
-print("Polling...")
+
+async def loop():
+    while True:
+        message = p.get_message()
+        if message and message["type"] not in ("subscribe", "unsubscribe"):
+            await bot.send_message(ch_id, message["data"].decode('utf-8'))
+        await asyncio.sleep(0.01)
+
+async def main():
+    await asyncio.gather(loop(), bot.polling())
+asyncio.run(main())
 
